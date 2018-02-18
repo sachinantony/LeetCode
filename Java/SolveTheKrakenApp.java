f8ab565699320f1ee3d8f2067c82623fd10cbd73
Sachin Antony <sachinantony007@gmail.com>
2018-02-18 00:36:17 -0500

    Committed the solve karaken program

---
+++ Java/SolveTheKrakenApp.java
@@ -0,0 +1,25 @@
+public class SolveTheKrakenApp
+{
+    public static void main(String[] args) {
+        System.out.println(krakenCount(2,3));
+    }
+    static long krakenCount(int m, int n){
+        long[][] count = new long[m][n];
+
+        for(int i = 0; i < m; i++){
+            count[i][0] = 1;
+        }
+
+        for(int i = 0; i < n; i++){
+            count[0][i] = 1;
+        }
+
+        for(int i = 1; i< m; i++){
+            for(int j = 1; j < n; j++){
+                count[i][j] = count[i-1][j] + count[i][j-1] +count[i-1][j-1];
+            }
+        }
+        return count[m-1][n-1];
+    }
+
+}
